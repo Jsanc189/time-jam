c3004adcb0610559d8ab0bb06df80cf64e21a189
Jsanc189 <jackie.sanchez0387@gmail.com>
2026-04-08 13:46:53 -0700

    Update Credits.js

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -14,11 +14,35 @@ export default class CreditsScene extends Phaser.Scene{
 
     create() {
         this.cameras.main.setBackgroundColor('#6e3318');
+
+        //credits to display in the credits scene
+        this.credits = [
+            'Producer:\n\nJackie Sanchez',
+            'Game Designers:\n\nRaven Cruz\n\nJackie Sanchez',
+            'Programmers:\n\nRaven Cruz\n\nJackie Sanchez',
+            'Lead Artist:\n\nSunny Lee',
+            'Character Artist:\n\nMars',
+            'Music Designer:\n\nSimon Blidener',
+            'SFX Designer:\n\nSimon Blidener',
+            'Special Thanks: '
+        ]
+
+        this.currentCreditIndex = 0;
+        
+        this.creditScrollText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, this.credits[this.currentCreditIndex],
+            { 
+                fontSize: '48px', 
+                fill: '#fff', 
+                align: 'center' 
+            }).setOrigin(0.5).setAlpha(0);
+        
+        this.showNextCredit();
+        
+        
+        
         //button to get back to MenuScene
         const BUTTON_SPACING = 100;
-        const TITLE_TEXT = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Credits',
-            { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-        const MENU_BUTTON = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + BUTTON_SPACING, 'Back to Menu', 
+        const MENU_BUTTON = this.add.text(this.cameras.main.centerX * 1.5, this.cameras.main.centerY * 1.5 + BUTTON_SPACING, 'Back to Menu', 
             { 
                 fontSize: '32px',
                 backgroundColor: '#fff', 
@@ -34,6 +58,36 @@ export default class CreditsScene extends Phaser.Scene{
         });
         MENU_BUTTON.on('pointerdown', () =>{
             this.scene.start('MenuScene');
+        });
+    }
+
+    showNextCredit() {
+        //go back to menu scene after all credits have been shown
+        if (this.currentCreditIndex >= this.credits.length) {
+            this.scene.start('MenuScene');
+            return;
+        }
+
+        const text = this.credits[this.currentCreditIndex];
+        this.creditScrollText.setText(text);
+
+        this.tweens.add({
+            targets: this.creditScrollText,
+            alpha: 1,
+            duration: 2000,
+            onComplete: () => {
+                this.time.delayedCall(2000, () => {
+                     this.tweens.add({
+                        targets: this.creditScrollText,
+                        alpha: 0,
+                        duration: 2000,
+                        onComplete: () => {
+                            this.currentCreditIndex++;
+                            this.showNextCredit();
+                        }
+                    });
+                });
+            }
         });
     }
 }
