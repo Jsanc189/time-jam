dfa5e4abb0ee78e346ec0e77b53c92891502f451
Sunny Han <shan66@ucsc.edu>
2026-04-13 23:42:26 -0700

    Button class
    Co-authored-by: Jsanc189 <Jsanc189@users.noreply.github.com>

---
+++ src/prefabs/Button.js
@@ -0,0 +1,83 @@
+/*
+    Created by: Sunny Han & Jackie Sanchez
+    Date Created: 4/13/2026
+    Updated by: Sunny Han
+    Description: This is a Button prefab to use for the game.  It takes in a scene, x and y coordinates,
+    a key for the texture, and a callback function to execute when the button is clicked.  
+*/
+
+import Phaser from 'phaser';
+import GameText from './GameText';
+
+export default class Button extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, width, height, text, textStyles, backgroundStyles, callback) {
+        const finalTextStyles = { color: '#160402', ...textStyles };
+        const finalBackgroundStyles = {
+            backgroundColor: 0xe9cda2,
+            borderColor: 0x43282b,
+            ...backgroundStyles,
+        };
+        super(scene, x, y);
+        this.scene.add.existing(this);
+        // needs padding because generateTexture cuts the line stroke off
+        const PADDING = 20;
+        width = width + PADDING;
+        height = height + PADDING;
+
+        // create the button background
+        const buttonBG = scene.make.graphics({ x: 0, y: 0, add: false });
+        buttonBG.lineStyle(16, finalBackgroundStyles.borderColor);
+        buttonBG.strokeRoundedRect(PADDING / 2, PADDING / 2, width - PADDING, height - PADDING, {
+            tl: 16,
+            tr: 16,
+            bl: 16,
+            br: 16,
+        });
+        buttonBG.fillStyle(finalBackgroundStyles.backgroundColor, 1);
+        buttonBG.fillRoundedRect(PADDING / 2, PADDING / 2, width - PADDING, height - PADDING, 16);
+        buttonBG.generateTexture('Button', width, height);
+        // make lighter background for hover effect
+        const hoverColor = Phaser.Display.Color.ValueToColor(
+            finalBackgroundStyles.backgroundColor,
+        ).brighten(8).color;
+        buttonBG.fillStyle(hoverColor, 1);
+        buttonBG.fillRoundedRect(PADDING / 2, PADDING / 2, width - PADDING, height - PADDING, 16);
+        buttonBG.generateTexture('ButtonHover', width, height);
+        // make darker background for click effect
+        const clickColor = Phaser.Display.Color.ValueToColor(
+            finalBackgroundStyles.backgroundColor,
+        ).darken(32).color;
+        buttonBG.fillStyle(clickColor, 1);
+        buttonBG.fillRoundedRect(PADDING / 2, PADDING / 2, width - PADDING, height - PADDING, 16);
+        buttonBG.generateTexture('ButtonClick', width, height);
+
+        this.newButton = scene.add.image(x, y, 'Button').setOrigin(0.5, 0.5);
+
+        this.buttonText = new GameText(scene, x, y, text, {
+            ...finalTextStyles,
+        }).setOrigin(0.5, 0.5);
+
+        // button hover effect
+        let isDown = false;
+        this.newButton
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => {
+                isDown = true;
+                this.newButton.setTexture('ButtonClick');
+            })
+            .on('pointerup', () => {
+                if (isDown) {
+                    callback(); // only fires if it was actually pressed
+                }
+                isDown = false;
+                this.newButton.setTexture('ButtonHover');
+            })
+            .on('pointerout', () => {
+                isDown = false;
+                this.newButton.setTexture('Button');
+            })
+            .on('pointerover', () => {
+                if (!isDown) this.newButton.setTexture('ButtonHover');
+            });
+    }
+}
--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -5,49 +5,77 @@
     
 */
 
-import GameText from "../prefabs/GameText";
+import GameText from '../prefabs/GameText';
+import Button from '../prefabs/Button';
 
-
-export default class MainScene extends Phaser.Scene{
+export default class MainScene extends Phaser.Scene {
     constructor() {
         super('MainScene');
     }
 
-    
     create() {
         this.cameras.main.setBackgroundColor('#6e3318');
         //button to get back to MenuScene
         const BUTTON_SPACING = 150;
-        const TITLE_TEXT = new GameText(this, this.cameras.main.centerX, this.cameras.main.centerY - BUTTON_SPACING, 'Main Game Scene', {
-            fontSize: '128px',
-            color: '#fff'
-        }).setOrigin(0.5);
-            
-        const MENU_BUTTON = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Back to Menu', 
-            { 
-                fontSize: '64px',
-                backgroundColor: '#fff', 
-                color: '#338de1',
-                padding: {x:20 , y: 10} 
-            }).setOrigin(0.5).setInteractive();
-
-        MENU_BUTTON.on('pointerover', () =>{
-            MENU_BUTTON.setStyle({ backgroundColor: '#338de1', color: '#fff' });
-        });
-        MENU_BUTTON.on('pointerout', () =>{
-            MENU_BUTTON.setStyle({ backgroundColor: '#fff', color: '#338de1' });
-        });
-        MENU_BUTTON.on('pointerdown', () =>{
-            this.scene.start('MenuScene');
-        });
-
-        const MAP_BUTTON = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + BUTTON_SPACING, 'Go to Mind Palace',
+        const TITLE_TEXT = new GameText(
+            this,
+            this.cameras.main.centerX,
+            this.cameras.main.centerY - BUTTON_SPACING,
+            'Main Game Scene',
             {
-                fontSize: '64px',
-                backgroundColor: '#fff',
-                color: '#338de1',
-                padding: { x: 20, y: 10 }
-            }).setOrigin(0.5).setInteractive();
+                fontSize: '128px',
+                color: '#fff',
+            },
+        ).setOrigin(0.5);
+
+        // const MENU_BUTTON = this.add
+        //     .text(this.cameras.main.centerX, this.cameras.main.centerY, 'Back to Menu', {
+        //         fontSize: '64px',
+        //         backgroundColor: '#fff',
+        //         color: '#338de1',
+        //         padding: { x: 20, y: 10 },
+        //     })
+        //     .setOrigin(0.5)
+        //     .setInteractive();
+
+        const MENU_BUTTON = new Button(
+            this,
+            this.cameras.main.centerX,
+            this.cameras.main.centerY,
+            300,
+            100,
+            'Back to Menu',
+            undefined,
+            undefined,
+            () => {
+                this.scene.start('MenuScene');
+            },
+        );
+
+        // MENU_BUTTON.on('pointerover', () => {
+        //     MENU_BUTTON.setStyle({ backgroundColor: '#338de1', color: '#fff' });
+        // });
+        // MENU_BUTTON.on('pointerout', () => {
+        //     MENU_BUTTON.setStyle({ backgroundColor: '#fff', color: '#338de1' });
+        // });
+        // MENU_BUTTON.on('pointerdown', () => {
+        //     this.scene.start('MenuScene');
+        // });
+
+        const MAP_BUTTON = this.add
+            .text(
+                this.cameras.main.centerX,
+                this.cameras.main.centerY + BUTTON_SPACING,
+                'Go to Mind Palace',
+                {
+                    fontSize: '64px',
+                    backgroundColor: '#fff',
+                    color: '#338de1',
+                    padding: { x: 20, y: 10 },
+                },
+            )
+            .setOrigin(0.5)
+            .setInteractive();
 
         MAP_BUTTON.on('pointerover', () => {
             MAP_BUTTON.setStyle({ backgroundColor: '#338de1', color: '#fff' });
@@ -58,10 +86,7 @@ export default class MainScene extends Phaser.Scene{
         MAP_BUTTON.on('pointerdown', () => {
             this.scene.start('MapScene');
         });
-
     }
 
-    update(){
-    }
-    
+    update() {}
 }
