9c51ef9512b665e0f1c7cccc71f3ed5cd81b1d45
Jsanc189 <jackie.sanchez0387@gmail.com>
2026-04-21 11:17:26 -0700

    comments out test code

--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -128,82 +128,82 @@ export default class MainScene extends Phaser.Scene {
     }
 
     // TEMP TESTING CODE
-    testObjectives(){
-        const PLAYER_ROLE_OG = this.case.playerRole;    // preserve
+    // testObjectives(){
+    //     const PLAYER_ROLE_OG = this.case.playerRole;    // preserve
 
-        const LOG_ID = "[Main] testObjectives()";
-        const pass = (msg) => console.log(`${LOG_ID} PASS: ${msg}`);
-        const fail = (msg) => console.error(`${LOG_ID} FAIL: ${msg}`);
-        const assert = (condition, msg) => condition ? pass(msg) : fail(msg);
-        const header = (title) => console.log(`${'_'.repeat(40)}\n--- ${title}\n${'_'.repeat(40)}`);
+    //     const LOG_ID = "[Main] testObjectives()";
+    //     const pass = (msg) => console.log(`${LOG_ID} PASS: ${msg}`);
+    //     const fail = (msg) => console.error(`${LOG_ID} FAIL: ${msg}`);
+    //     const assert = (condition, msg) => condition ? pass(msg) : fail(msg);
+    //     const header = (title) => console.log(`${'_'.repeat(40)}\n--- ${title}\n${'_'.repeat(40)}`);
 
-        const testRole = (role) => {
-            header(`\n${LOG_ID} ... Initial Build ... testing ${role}`);
-            this.case.playerRole = role;
-            const ctrl = new ObjectivesController(this.case, NUM_OBJECTIVES);
-            const all = ctrl.getAll();
+    //     const testRole = (role) => {
+    //         header(`\n${LOG_ID} ... Initial Build ... testing ${role}`);
+    //         this.case.playerRole = role;
+    //         const ctrl = new ObjectivesController(this.case, NUM_OBJECTIVES);
+    //         const all = ctrl.getAll();
 
-            assert(all.length > 0, `${LOG_ID} ... Generated ${ctrl.getAll().length} ${this.case.role} objectives`);
-            assert(ctrl.getPending().length === all.length, `${LOG_ID} ... All ${all.length} start as pending`);
+    //         assert(all.length > 0, `${LOG_ID} ... Generated ${ctrl.getAll().length} ${this.case.role} objectives`);
+    //         assert(ctrl.getPending().length === all.length, `${LOG_ID} ... All ${all.length} start as pending`);
 
-            // should always generate crime scene objectives
-            const crimeSceneObj = all.find((o) => o.id === "crime_scene");
-            assert(!!crimeSceneObj, `Crime scene objective spawns`);
+    //         // should always generate crime scene objectives
+    //         const crimeSceneObj = all.find((o) => o.id === "crime_scene");
+    //         assert(!!crimeSceneObj, `Crime scene objective spawns`);
 
-            // suspect investigation objectives spawn, capped at NUM_OBJECTIVES
-            const susObjs = all.filter((o) => o.suspect);
-            assert(susObjs.length <= NUM_OBJECTIVES, `${LOG_ID} ... Suspect objectives (${susObjs.length}) do not exceed NUM_OBJECTIVES (${NUM_OBJECTIVES})`);
+    //         // suspect investigation objectives spawn, capped at NUM_OBJECTIVES
+    //         const susObjs = all.filter((o) => o.suspect);
+    //         assert(susObjs.length <= NUM_OBJECTIVES, `${LOG_ID} ... Suspect objectives (${susObjs.length}) do not exceed NUM_OBJECTIVES (${NUM_OBJECTIVES})`);
 
-            const defendant = this.case.defendant;         
-            if(role == PROSECUTE_ROLE){
-                // prosecution should only ever target defendant
-                //const wrongSuspect = roleObjs.find((o) => )
-            }
+    //         const defendant = this.case.defendant;         
+    //         if(role == PROSECUTE_ROLE){
+    //             // prosecution should only ever target defendant
+    //             //const wrongSuspect = roleObjs.find((o) => )
+    //         }
 
-            console.log(`${LOG_ID} ... SUMMARY (${this.case.playerRole}):`, ctrl.getSummary());
-        }
+    //         console.log(`${LOG_ID} ... SUMMARY (${this.case.playerRole}):`, ctrl.getSummary());
+    //     }
 
-        // testing roles
-        testRole(PROSECUTE_ROLE);
-        testRole(DEFENSE_ROLE);
+    //     // testing roles
+    //     testRole(PROSECUTE_ROLE);
+    //     testRole(DEFENSE_ROLE);
 
-        // should always generate crime scene objectives
-        const crimeSceneObj = allD.find((o) => o.id === "crime_scene");
-        assert(!!crimeSceneObj, `Crime scene objective spawns`);
+    //     // should always generate crime scene objectives
+    //     const crimeSceneObj = allD.find((o) => o.id === "crime_scene");
+    //     assert(!!crimeSceneObj, `Crime scene objective spawns`);
 
-        // suspect investigation objectives spawn, capped at NUM_OBJECTIVES
-        const susObjs = allD.filter((o) => o.suspect);
-        assert(susObjs.length <= NUM_OBJECTIVES, `${LOG_ID} ... Suspect objectives (${susObjs.length}) do not exceed NUM_OBJECTIVES (${NUM_OBJECTIVES})`);
+    //     // suspect investigation objectives spawn, capped at NUM_OBJECTIVES
+    //     const susObjs = allD.filter((o) => o.suspect);
+    //     assert(susObjs.length <= NUM_OBJECTIVES, `${LOG_ID} ... Suspect objectives (${susObjs.length}) do not exceed NUM_OBJECTIVES (${NUM_OBJECTIVES})`);
 
-        console.log(`${LOG_ID} ... SUMMARY (${this.case.playerRole}):`, ctrlD.getSummary());
-
-
+    //     console.log(`${LOG_ID} ... SUMMARY (${this.case.playerRole}):`, ctrlD.getSummary());
 
 
 
 
-        header(`${LOG_ID} ... Triggers`);
-        const roomHit = ctrl.onRoomVisited(this.case.crime.scene);
-        assert(roomHit.length > 0, `${LOG_ID} ... onRoomVisited(crime scene) completed ${roomHit.length} objective(s)`);
-        assert(ctrl.onRoomVisited(this.case.crime.scene).length === 0, `${LOG_ID} ... Re-visiting completes nothing`);
 
-        const itemHit = ctrl.onItemFound(this.case.crime.object);
-        assert(itemHit.length > 0, `${LOG_ID} ... onItemFound(crime object) completed ${itemHit.length} objective(s)`);
-        assert(ctrl.onItemFound(this.case.crime.object).length === 0, `${LOG_ID} ... Re-finding completes nothing`);
 
-        const motiveHit = ctrl.onMotiveUncovered(this.case.defendant.name);
-        assert(motiveHit.length > 0, `${LOG_ID} ... onMotiveUncovered(defendant) completed ${motiveHit.length} objective(s)`);
+    //     header(`${LOG_ID} ... Triggers`);
+    //     const roomHit = ctrl.onRoomVisited(this.case.crime.scene);
+    //     assert(roomHit.length > 0, `${LOG_ID} ... onRoomVisited(crime scene) completed ${roomHit.length} objective(s)`);
+    //     assert(ctrl.onRoomVisited(this.case.crime.scene).length === 0, `${LOG_ID} ... Re-visiting completes nothing`);
 
-        header(`${LOG_ID} ... role = prosecution`);
-        ctrl.applyRole('prosecution');
-        assert(ctrl.getByCategory('role').length > 0, `${LOG_ID} ... Role objectives added`);
-        assert(ctrl.isComplete('prosecution_build_case') === false, `${LOG_ID} ... prosecution_build_case starts pending`);
+    //     const itemHit = ctrl.onItemFound(this.case.crime.object);
+    //     assert(itemHit.length > 0, `${LOG_ID} ... onItemFound(crime object) completed ${itemHit.length} objective(s)`);
+    //     assert(ctrl.onItemFound(this.case.crime.object).length === 0, `${LOG_ID} ... Re-finding completes nothing`);
 
-        console.log(`\n${LOG_ID} ...  Final summary:`, ctrl.getSummary());
-        console.log(`${LOG_ID} ... All objectives:`, ctrl.getAll());
+    //     const motiveHit = ctrl.onMotiveUncovered(this.case.defendant.name);
+    //     assert(motiveHit.length > 0, `${LOG_ID} ... onMotiveUncovered(defendant) completed ${motiveHit.length} objective(s)`);
 
-        this.case.playerRole = PLAYER_ROLE_OG;    // revert
-    }
+    //     header(`${LOG_ID} ... role = prosecution`);
+    //     ctrl.applyRole('prosecution');
+    //     assert(ctrl.getByCategory('role').length > 0, `${LOG_ID} ... Role objectives added`);
+    //     assert(ctrl.isComplete('prosecution_build_case') === false, `${LOG_ID} ... prosecution_build_case starts pending`);
+
+    //     console.log(`\n${LOG_ID} ...  Final summary:`, ctrl.getSummary());
+    //     console.log(`${LOG_ID} ... All objectives:`, ctrl.getAll());
+
+    //     this.case.playerRole = PLAYER_ROLE_OG;    // revert
+    // }
 
     update() {}
 }
